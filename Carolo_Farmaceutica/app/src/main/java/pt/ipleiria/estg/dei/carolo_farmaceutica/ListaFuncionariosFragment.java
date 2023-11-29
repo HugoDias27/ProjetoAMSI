2cd9b6ca2b9792f082c159f5a5c65978c2ba1f05
HugoDias27 <2220853@my.ipleiria.pt>
2023-11-29 15:37:17 +0000

    layout descontos

--- Carolo_Farmaceutica/app/src/main/java/pt/ipleiria/estg/dei/carolo_farmaceutica/ListaFuncionariosFragment.java
+++ Carolo_Farmaceutica/app/src/main/java/pt/ipleiria/estg/dei/carolo_farmaceutica/ListaFuncionariosFragment.java
@@ -9,19 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class Homepage extends Fragment {
+public class ListaFuncionariosFragment extends Fragment {
 
 
-    public Homepage() {
+    public ListaFuncionariosFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homepage, container, false);
+        return inflater.inflate(R.layout.fragment_lista__funcionarios, container, false);
     }
 }
